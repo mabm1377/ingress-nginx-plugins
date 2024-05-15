07ceec8ffcfb4c3dd3466c07299cd8826318d498
mabm1377 <36042841+mabm1377@users.noreply.github.com>
2024-05-15 11:02:13 +0330

    Update main.lua

--- hello_world/main.lua
+++ hello_world/main.lua
@@ -6,15 +6,7 @@ local ngx_log = ngx.log
 local _M = {}
 
 function _M.rewrite()
-  local ua = ngx.var.http_user_agent
-  ngx_log(ngx.NOTICE, string_format("User Agent passed: \"%s\"", ua))
-  if ua == "hello" then
-    ngx_log(ngx.NOTICE, string_format("User Agent matched 'hello'"))
-    ngx.req.set_header("x-hello-world", "hello")
-  else
-    ngx_log(ngx.NOTICE, string_format("User Agent did not match any provided conditions"))
-    ngx.req.set_header("x-hello-world", "unknwon")
-  end
+  ngx.say("test")
 end
 
 return _M
